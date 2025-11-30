143bba0bc3479ac043d545628c34ba5d30d318c4
Vicillian <vic.liu101@gmail.com>
2025-11-29 23:44:05 -0800

    Lab 9
    Not many but a few changes made.

--- exercises/lab04.js
+++ exercises/lab04.js
@@ -11,10 +11,11 @@ let myFavouriteCommute = {
     fullness: 50
 };
 
+
 let megaSentence;
 
 megaSentence = "<p>My two top commutes from the array are: " + myCommutes[0] + ", " + myCommutes[5] + "</p>";
 
 megaSentence = megaSentence + "<p>My favourite commute possesses such characteristics: type - " + myFavouriteCommute.type + ", route number " + myFavouriteCommute.route + ", the best driver: " + myFavouriteCommute.drivers[0] + "</p>";
 
-$("#output").html(megaSentence);
+$("#output").html(megaSentence); //gonna be real honest idk what more i can change man like all my code is fine. i read the articles but like they dont really apply here
